de74dd139299c19fc204e0b8da353e94f8f8b92b
PV-NIKITHA <nikki021999@gmail.com>
2021-10-24 17:44:24 +0530

    Js

---
+++ modulus.js
@@ -0,0 +1,12 @@
+ const num1=5;
+ const num2=7;
+
+ const rem=num1%num2;
+ if(rem%2===0)
+ {
+     console.log(rem,"is divisible bt 2");
+ } 
+ else{
+    console.log(rem,"is not divisible bt 2");
+ }
+ 
